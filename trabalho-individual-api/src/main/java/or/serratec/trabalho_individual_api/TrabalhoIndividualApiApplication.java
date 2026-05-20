5a37dd13af55b520e466ce6d8bbbf45dcaeed7b7
brunovitor-git05 <brunovazvitor@gmail.com>
2026-05-19 22:37:53 -0300

    Implementando as dependencias obrigatórias do projeto

---
+++ trabalho-individual-api/src/main/java/or/serratec/trabalho_individual_api/TrabalhoIndividualApiApplication.java
@@ -0,0 +1,13 @@
+package or.serratec.trabalho_individual_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoIndividualApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoIndividualApiApplication.class, args);
+	}
+
+}
---
+++ trabalho-individual-api/src/test/java/or/serratec/trabalho_individual_api/TrabalhoIndividualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package or.serratec.trabalho_individual_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoIndividualApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
